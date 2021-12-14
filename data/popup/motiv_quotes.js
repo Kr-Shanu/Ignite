5070fd3c2f32a1a99a596e7a124377f78a16a547
priyam <priyamshankar.5@gmail.com>
2021-12-14 13:24:40 +0530

    motivational quote.js cleaning

--- data/popup/motiv_quotes.js
+++ data/popup/motiv_quotes.js
@@ -1,16 +1,18 @@
+
+// We are selecting two buttons using the id motivation on and off and then we are calling two function
+// to trigger it and switch it off.
 document.getElementById("motivation_on").addEventListener("click", newQuote);
 document.getElementById("motivation_off").addEventListener("click", clear_quote);
-// document.getElementById("focus_mode").addEventListener("click",foucsmode_trigger);
 
+// function to stop motivational quotes
 function clear_quote() {
     chrome.runtime.sendMessage({
         greeting: "clear_motivation"
     })
-    // alert("press");
 }
 
+// Function to generate two motivational quotes.
 function newQuote() {
-    const minutes = 1.0;
     chrome.runtime.sendMessage({
         greeting: "put_motivation"
     })
